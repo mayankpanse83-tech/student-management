ab0c8dd1e661a9f54aa88d17ee83e628867e5540
mayankpanse83-tech <mayankpanse83@gmail.com>
2026-07-25 18:21:35 +0530

    My first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+function Sidebar() {
+  return (
+    <div className="sidebar">
+
+      <h2>EMS</h2>
+
+      <ul>
+        <li>Dashboard</li>
+        <li>Attendance</li>
+        <li>Daily Updates</li>
+        <li>Tasks</li>
+        <li>Leave</li>
+        <li>Reports</li>
+        <li>Profile</li>
+        <li>Notifications</li>
+        <li>Employees</li>
+        <li>Settings</li>
+      </ul>
+
+    </div>
+  );
+}
+
+export default Sidebar;
