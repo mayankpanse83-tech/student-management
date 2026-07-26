c3c0f23e5b1076df6a3da9d4017bc5ff685512ff
mayankpanse83-tech <mayankpanse83@gmail.com>
2026-07-26 17:48:35 +0530

    Change in main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,15 @@
-// This is main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// Global CSS
+import "./index.css";
+
+// Sidebar CSS (agar alag file banayi hai)
+import "./components/Sidebar.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
